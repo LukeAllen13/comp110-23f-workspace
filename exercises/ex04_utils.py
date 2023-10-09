975a2d40f75b52862786e42b18494dfd6ff9d86c
Luke Allen <apl1@unc.edu>
2023-10-09 12:35:14 -0400

    utils

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,6 +1,7 @@
 """EX04 - List Utility Functions!"""
 __author__ = "730704135"
 
+
 def all(int_list: list[int], single_int: int) -> bool:
     """Given a list of integers and a sngle integer, check to see if the list contains only the single integer. If so, return True, if not, return False."""
     i = 0
@@ -12,19 +13,21 @@ def all(int_list: list[int], single_int: int) -> bool:
             return False
     return True
 
+
 def max(ints: list[int]) -> int:
     """Given a list of integers, return the integer that is the largest."""
     i = 0
     # uses the index counter i to go through each int in the list and compare it to the int in the next index. 
     # I did len (ints) -1 because you dont need to compare the int of the second to last index to anything if you already have the resulting info.
-    max_int:int = 0
+    max_int: int = 0
     if len(ints) == 0:
         raise ValueError("max() arg is an empty List")
     while i < len(ints) - 1:
         if ints[i] > ints[i + 1]:
-            max_int = int[i]
+            max_int = ints[i]
         i += 1
     return max_int
+
 
 def is_equal(list1: list[int], list2: list[int]) -> bool:
     """Given two lists of integers, return True is the lists are exactly the same."""
@@ -38,5 +41,3 @@ def is_equal(list1: list[int], list2: list[int]) -> bool:
         else:
             return False
     return True
-
-
