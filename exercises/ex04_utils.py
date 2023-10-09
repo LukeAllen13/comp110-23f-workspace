e34ed7d341bce70014a8f0e311c2b99b71a4b470
Luke Allen <apl1@unc.edu>
2023-10-09 11:35:47 -0400

    utils

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,7 +1,7 @@
 """EX04 - List Utility Functions!"""
 __author__ = "730704135"
 
-def all_same(int_list: list, single_int: int) -> bool:
+def all(int_list: list[int], single_int: int) -> bool:
     """Given a list of integers and a sngle integer, check to see if the list contains only the single integer. If so, return True, if not, return False."""
     i = 0
     while i < int(len(int_list)):
@@ -10,5 +10,16 @@ def all_same(int_list: list, single_int: int) -> bool:
         else:
             return False
     return True
+
+def max(ints: list[int]) -> int:
+    """Given a list of integers, return the integer that is the largest."""
+    i = 0
+    max_int:int = 0
+    if len(ints) == 0:
+        raise ValueError("max() arg is an empty List")
+    while i < len(ints) - 1:
+        if ints[i + 1] > ints[i]:
+            max_int = int[i + 1]
+        i += 1
 
 
