307e800eb32676c02b85f147ea27b2950c2b3aa1
Luke Allen <apl1@unc.edu>
2023-10-10 16:00:45 -0400

    lists

--- lessons/lists.py
+++ lessons/lists.py
@@ -7,3 +7,13 @@ grocery_list.append("bread")
 grocery_list[1]: str = "cereal"
 grocery_list.pop(2)
 print(grocery_list)
+
+x: list[str] = ["Comp", "110"]
+x[1] = "210"
+y: str = x
+print(y)
+
+a: str = "24"
+b: str = a 
+a += "6"
+print(b)
