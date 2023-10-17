2c73526ea6f26b96203406c5eca8c1bb0f070d01
Luke Allen <apl1@unc.edu>
2023-10-17 10:25:04 -0400

    sum

--- lessons/sum.py
+++ lessons/sum.py
@@ -2,6 +2,8 @@
 
 
 def w_sum(nums: list[int]) -> int:
+    """sums each integer in an inputed list."""
+    assert len(nums) > 0
     i = 0
     sum: int = 0
     while i < len (nums):
@@ -11,6 +13,8 @@ def w_sum(nums: list[int]) -> int:
 
 
 def f_sum(nums: list[int]) -> int:
+    """sums each integer in an inputed list."""
+    assert len(nums) > 0
     sum: int = 0
     for num in nums:
         sum += num
@@ -18,6 +22,8 @@ def f_sum(nums: list[int]) -> int:
 
 
 def f_range_sum(nums: list[int]) -> int:
+    """sums each index in an inputed list."""
+    assert len(nums) > 0
     sum: int = 0
     for idx in range(0, len(nums)):
         sum += nums[idx]
