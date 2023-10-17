55a2b9f3300c886cc52484206de43da47d8da9a1
Luke Allen <apl1@unc.edu>
2023-10-17 10:56:22 -0400

    sum

--- lessons/sum.py
+++ lessons/sum.py
@@ -21,10 +21,15 @@ def f_sum(nums: list[int]) -> int:
     return sum
 
 
-def f_range_sum(nums: list[int]) -> int:
+print("Input a list of ints into the function f_range_sum. Try to input ints that add up to my mystery lucky number!")
+def f_range_sum(nums: list[int]) -> int or str:
     """sums each index in an inputed list."""
     assert len(nums) > 0
+    lucky_number = 13
     sum: int = 0
     for idx in range(0, len(nums)):
         sum += nums[idx]
-    return sum
+    if sum == lucky_number:
+        return "YOU FOUND MY LUCKY NUMBER!!"
+    if sum != lucky_number:
+        return (f"Try again! My lucky number is not {sum}")
