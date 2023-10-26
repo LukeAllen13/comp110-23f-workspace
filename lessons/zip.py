7b154a22a3346aac4b2e0c353161fd5a3acf0926
Luke Allen <apl1@unc.edu>
2023-10-26 10:16:26 -0400

    zip

--- lessons/zip.py
+++ lessons/zip.py
@@ -1,13 +1,16 @@
 """Combining two lists into a dictionary."""
 __author__ = "730704135"
 
+
 def zip(key: list[str], value: list[int]) -> dict[str, int]:
-    zippy: dict[str, int] = dict(key, value)
+    """Assign values to keys which are inputs through 2 lists using the function zip."""
+    zippy: dict[str, int] = {}
     if len(key) and len(value) == 0:
-        return dict()
+        return zippy
     if len(key) != len(value):
-        return dict()
+        return zippy
     i = 0
     while i < len(key):
         zippy[f"{key[i]}"] = value[i]
-    return dict
+        i += 1
+    return zippy
