3be13606b24c738da17aa4b061b6cd8f555c67d0
Luke Allen <apl1@unc.edu>
2023-11-17 14:54:42 -0500

    node

--- lessons/recursion/node.py
+++ lessons/recursion/node.py
@@ -21,12 +21,20 @@ class Node:
         else:
             return f"{self.data} -> {self.next}"
         
-    def head(self) -> int:
-        
-        return None
+    def head(self) -> int | None:
+        """Return the data in the first node of the linked list."""
+        return self.data
     
-    def tail(self):
-        return None
+    def tail(self) -> Node | None:
+        """Return a linked list of every element except for the head."""
+        return self.next
     
-    def last(self):
-        return None
+    def last(self) -> int | None:
+        """Return the data in the last node of the linked list."""
+        tail_node = self.tail()
+        if tail_node != None:
+            while tail_node.next != None:
+                tail_node = tail_node.next
+            return tail_node.data
+        else:
+            return None
