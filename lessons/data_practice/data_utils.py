c71b89eda203b2057f3532fccf05a70b5b8dd1d9
Luke Allen <apl1@unc.edu>
2023-11-30 10:03:47 -0500

    data

---
+++ lessons/data_practice/data_utils.py
@@ -0,0 +1,9 @@
+"""Working with CSV Data."""
+
+from csv import DictReader
+
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Read a csv file and return as a list of header keys."""
+    result: list[dict[str, str]] = []
+    
+    return result
